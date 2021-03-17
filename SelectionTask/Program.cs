dbe6929313b139b9b41c946d0beaef6c3247f132
GritsadaHatton <102317760@student.swin.edu.au>
2021-03-17 14:53:03 +1100

    Initial commit

---
+++ SelectionTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        Console.Write("Please enter your age: ");
+        string age = Console.ReadLine();
+
+        int ageNum = int.Parse(age);
+
+        if (ageNum > 17)
+            {
+                Console.WriteLine("Legally Adult Age");
+            }
+            else
+            {
+                Console.WriteLine("Leagally Not Adult Age");
+            }
+
+
+
+        }
+    }
+}
