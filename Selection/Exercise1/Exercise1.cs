8fa1c00bea7bce38cc445314cd7cda7ab9088aed
GritsadaHatton <102317760@student.swin.edu.au>
2021-03-17 17:41:19 +1100

    Commit

---
+++ Selection/Exercise1/Exercise1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+          
+        
+        Console.Write("Please enter your age: ");
+        string age = Console.ReadLine();  
+        int ageNum = int.Parse(age);
+
+        Console.Write("Please enter your gender: ");
+        string gender = Console.ReadLine();
+
+        Console.Write("Please enter a random number between 1-10: ");
+        string Number = Console.ReadLine();
+        int Num = int.Parse(Number);
+
+        Console.Write("Please enter another random number between 1-10: ");
+        string Number2 = Console.ReadLine();
+        int Num2 = int.Parse(Number2);
+
+
+        Console.WriteLine(Num + Num2);
+
+        if (Num + Num2 == 10)
+            {
+                Console.Write("Sum of numbers is equal to 10, ");
+            }
+            else if (Num + Num2 > 10)
+            {
+            Console.Write("Sum of numbers is greater than 10, ");
+            }
+            else if (Num + Num2 < 10)
+            {
+            Console.Write("Sum of numbers is less than 10, ");
+            }
+
+
+        if (ageNum > 17)
+            {
+                Console.WriteLine(gender + " is an adult");
+            }
+            else
+            {
+                Console.WriteLine(gender + " is not an adult");
+            }
+
+        
+
+        }
+    }
+}
---
+++ Selection/Exercise2/Exercise2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Exercise2
+    {
+        static void Main(string[] args)
+        {
+          
+            
+        Console.WriteLine("Please enter username: ");
+        string username = Console.ReadLine();
+        int user = int.Parse(username);
+
+        Console.WriteLine("Now please enter password: ");
+        string password = Console.ReadLine();
+        int pass = int.Parse(password);
+        
+        Console.WriteLine(username);
+        Console.WriteLine(password);
+
+        if (user == 102317760)
+            {
+                if (pass == 1234)
+                { 
+                    Console.WriteLine("Login Successful");
+                }
+            }
+        else
+        {   
+            Console.WriteLine("Login Unsuccessful");
+        }
+            
+        
+
+
+
+
+
+        
+        }
+    }
+}
