f20e69ff87b288e027df1cd8b96b56653fe67179
GritsadaHatton <102317760@student.swin.edu.au>
2021-03-17 16:01:29 +1100

    Commit

--- Selection/Program.cs
+++ Selection/Program.cs
@@ -6,31 +6,25 @@ namespace SelectionTask
     {
         static void Main(string[] args)
         {
-            
+          
+        
         Console.Write("Please enter your age: ");
-        string age = Console.ReadLine();
-
+        string age = Console.ReadLine();  
         int ageNum = int.Parse(age);
 
         Console.Write("Please enter your gender: ");
         string gender = Console.ReadLine();
 
         Console.Write("Please enter a random number between 1-10: ");
-        
         string Number = Console.ReadLine();
-
         int Num = int.Parse(Number);
 
         Console.Write("Please enter another random number between 1-10: ");
         string Number2 = Console.ReadLine();
-
         int Num2 = int.Parse(Number2);
 
+
         Console.WriteLine(Num + Num2);
-
-
-
-
 
         if (Num + Num2 == 10)
             {
@@ -44,7 +38,6 @@ namespace SelectionTask
             {
             Console.Write("Sum of numbers is less than 10, ");
             }
-
 
 
         if (ageNum > 17)
