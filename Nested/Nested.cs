407c739d65aa5e78d6de9008ad79f052ac244c67
GritsadaH <79820142+GritsadaH@users.noreply.github.com>
2021-04-01 16:31:51 +1100

    Commit

---
+++ Nested/Nested.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Nested
+{
+    class Nested
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            int counter = 0;
+
+            while (counter <10)
+            {
+                counter++;
+                int counter2 = counter;
+
+                while (counter2 < 5)
+                {
+                    Console.Write(name);
+                    counter2++;
+                }
+                Console.WriteLine();
+                
+            }
+            Console.WriteLine("Goodbye " + name);
+        }
+    }
+}
