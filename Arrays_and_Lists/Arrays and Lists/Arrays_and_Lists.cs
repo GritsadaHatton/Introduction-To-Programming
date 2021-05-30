5cf77dbd102f3083fd2d3bb750a2e32a94bb61a0
GritsadaHatton <102317760@student.swin.edu.au>
2021-05-30 21:42:05 +1000

    forcecommit

---
+++ Arrays_and_Lists/Arrays and Lists/Arrays_and_Lists.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Arrays_and_Lists
+{
+    class Arrays_and_Lists
+    {
+        static void Main(string[] args)
+        {
+
+            //declaring array
+            int size = 5;
+            int[] numbers = new int[size];
+
+            numbers[2] = 38;
+            numbers[0] = 1;
+            numbers[1] = 2323;
+            numbers[3] = 86;
+            numbers[4] = 103;
+
+            int index = 0;
+        
+
+            while (index < numbers.Length)
+            {
+                System.Console.WriteLine(numbers[index]);
+
+                index++;
+            }
+
+            int[] scores = new int[10];
+            int total = 0;
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                scores[i] = input;
+                //total = total + input;
+                total += input;
+            }
+
+            System.Console.WriteLine("=================================================");
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                System.Console.WriteLine(scores[i]);
+            }
+
+            System.Console.WriteLine("Total is: " + total);
+
+
+        }
+    }
+}
---
+++ Arrays_and_Lists/Lists_Submittable/Lists_Submittable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lists_Submittable
+{
+    class Lists_Submittable
+    {
+        static void Main(string[] args)
+        {
+
+            int size = 5;
+
+            int[] numbers = new int [size];
+
+
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 4;
+            numbers[4] = 5;
+
+
+            int index = 0;
+
+
+            while(index < numbers.Length)
+            {
+                System.Console.WriteLine(numbers[index]);
+
+                index++;
+            }
+
+
+            // int input = 0;
+            // Console.WriteLine("Enter a number: ");
+            // Console.ReadLine();
+
+            // if (input <= numbers[index])
+            // {
+            //     Console.WriteLine("This number already exists");
+            // }
+
+            // else if (input > numbers[index])
+            // {
+            //     Console.WriteLine("Yay");
+            // }
+
+        }
+    }
+}
